9d4a8ba4e48284a7715cc68134d20321dd437675
Yajiv Luck <luckheenarain.y.pp@gmail.com>
2022-07-05 21:05:21 -0400

    Update

--- Animation.py
+++ Animation.py
@@ -6,7 +6,7 @@ import time
 #functions = [np.sin(t),np.cos(t),np.log(t),np.exp(t),np.tan(t),np.arccosh(t)]
 
 CURRENT = int(time.time())
-FRAMES = 200 #Number of frame per functions
+FRAMES = 100 #Number of frame per functions
 INTERVALS = 0.1  #Seconds between each frames
 TIME_CUT = 2 #Time between switch of functions
 
@@ -20,22 +20,28 @@ def init():
     return line,
 
 #Generate data from a function (Only care about the value of the function at the current time, past values don't need to be stored in memory -> yield)
-def data_gen(t=0, FRAMES = FRAMES):
+def data_gen(t=0):
     cnt = 0
     while cnt < FRAMES:
         cnt += 1
         t += INTERVALS
         yield t, function_to_animate(t)
         
+
+def data2_gen(t=0):
+    cnt = 0
+    while cnt < FRAMES:
+        cnt += 1
+        t += INTERVALS
+        yield t, np.cos(t)
+        
+        
 def function_to_animate(t):
+    return np.sin(t)
+
     
-    T = int(t)
-    
-    time_frame = [(T in range(0,TIME_CUT)), (T in range(TIME_CUT,2*TIME_CUT)), (T in range(2*TIME_CUT,3*TIME_CUT))]
-    functions = [np.sin(t),np.cos(t),np.log(t)]
-    
-    return np.select(time_frame,functions, default = 1/t)
    
+
 fig, ax = plt.subplots()
 line, = ax.plot([], [], lw=2)
 ax.grid()
@@ -56,6 +62,12 @@ def run(data):
         ax.figure.canvas.draw()
     line.set_data(xdata, ydata)
     
+    if t < xmin:
+        ax.set_xlim(-2*xmin, xmax)
+        ax.figure.canvas.draw()
+    line.set_data(xdata, ydata)
+    
+    
     if y>=ymax:
         ax.set_ylim(ymin, 2*ymax)
         ax.figure.canvas.draw()
@@ -66,47 +78,22 @@ def run(data):
         ax.figure.canvas.draw()
     line.set_data(xdata, ydata)
         
-
     return line,
 
-#fig defines the pyplot window that the animation will take place on
-#func is a function that will be called at every frame
-#frames is the source of data that will be passed onto func
-#Blit priorizes overlapping of multiple animation
-#Interval is delay between frames in milliseconds
-#init_func is a function called once that defines the frame of the animation
-    
-
-
-ani = animation.FuncAnimation(fig, func = run, frames = data_gen, blit=False, interval=INTERVALS,
+ani = animation.FuncAnimation(fig, func = run, frames = data_gen(), blit=True, interval=INTERVALS,
                       repeat=False, init_func=init)
+
+ani2 = animation.FuncAnimation(fig, func = run, frames = data2_gen(), blit=True, interval=INTERVALS,
+                      repeat=False, init_func=init)
+
 plt.show()
 
-
-#
-#fig = plt.figure()
-#fig.set_dpi(100)
-#fig.set_size_inches(7, 6.5)
-#
-#ax = plt.axes(xlim=(0, 10), ylim=(0, 10))
-#patch = plt.Circle((5, -5), 0.75, fc='y')
-#
-#def init():
-#    patch.center = (5, 5)
-#    ax.add_patch(patch)
-#    return patch,
-#
-#def animate(i):
-#    x, y = patch.center
-#    x = 5 + 3 * np.sin(np.radians(i))
-#    y = 5 + 3 * np.cos(np.radians(i))
-#    patch.center = (x, y)
-#    return patch,
-#
-#anim = animation.FuncAnimation(fig, animate, 
-#                               init_func=init, 
-#                               frames=360, 
-#                               interval=20,
-#                               blit=True)
-#
-#plt.show()
+"""
+fig defines the pyplot window that the animation will take place on
+func is a function that will be called at every frame
+frames is the source of data that will be passed onto func
+Blit priorizes overlapping of multiple animation
+Interval is delay between frames in milliseconds
+init_func is a function called once that defines the frame of the animation
+    
+"""
---
+++ Animation2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  5 14:42:54 2022
+
+@author: yajivluck
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.animation as animation
+
+fig = plt.figure()
+fig.set_dpi(100)
+fig.set_size_inches(7, 6.5)
+
+ax = plt.axes(xlim=(0, 10), ylim=(0, 10))
+patch = plt.Circle((5, -5), 0.75, fc='y')
+
+def init():
+    patch.center = (5, 5)
+    ax.add_patch(patch)
+    return patch,
+
+def animate(i):
+    x, y = patch.center
+    x = 5 + 3 * np.sin(np.radians(i))
+    y = 5 + 3 * np.cos(np.radians(i))
+    patch.center = (x, y)
+    return patch,
+
+anim = animation.FuncAnimation(fig, animate, 
+                               init_func=init, 
+                               frames=360, 
+                               interval=20,
+                               blit=True)
+
+plt.show()
